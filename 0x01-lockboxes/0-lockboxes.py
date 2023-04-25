3542f95f7d99304d4ac5d27710ed0d4655dfad5f
AGBAGHER JAPHET <ternguy1995@gmail.com>
2023-04-25 07:32:32 -0700

    done

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" check if all boxes in the list of list can be opened """
+
+
+def canUnlockAll(boxes):
+    """ determining if all boxes can be opened """
+    for key in range(1, len(boxes) - 1):
+        res = False
+        for index in range(len(boxes)):
+            res = key in boxes[index] and key != index
+            if res:
+                break
+        if res is False:
+            return res
+    return True
---
+++ 0x01-lockboxes/README.md
@@ -0,0 +1,1 @@
+ this is my first readme file for this projecy 0x01-lockboxes

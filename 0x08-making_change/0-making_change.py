434ce817e45ecaaab4d58b1c7987abe32848f9bc
AGBAGHER JAPHET <ternguy1995@gmail.com>
2023-06-14 02:23:16 -0700

    done

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
-'''Making a change
-'''
+"""
+Interview Question on: fewest number of coins needed to
+meet a given amount total
+"""
 
 
 def makeChange(coins, total):
-    '''given a total amount, determine the least amound if coins needed
-    '''
-    if total_amount <= 0:
+    """ fewest number of coins needed to meet total """
+    if total <= 0:
         return 0
-    coins = 0
-    coins_index = 0
-    sort_coins = sorted(coins, reverse=True)
-    cons_length = len(coins)
-    while total_amount > 0:
-        if coins_index >= cons_length:
-            return -1
-        if total_amount - sort_coins[coins_index] >= 0:
-            total_amount -= sort_coins[coins_index]
-            coins += 1
-        else:
-            coins_index += 1
-    return coins
+    # sort the coins in descending order
+    coins.sort(reverse=True)
+    change = 0
+    for coin in coins:
+        if total <= 0:
+            break
+        temp = total // coin
+        change += temp
+        total -= (temp * coin)
+    if total != 0:
+        return -1
+    return change
